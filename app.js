55bf3ff9ffe98d1f57ae3cfcfead6477aa6b8714
Shabash Khan <shabashkhan.dev@gmail.com>
2022-10-08 00:28:10 +0530

    minor

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ var outputBox = document.querySelector("#output-Box");
 function calcProfitAndLoss(initial,quantity,current){
     if(initial>current){
     var loss = (initial-current)*quantity;
-    var lossPercentage = (loss/initial)*100;
+    var lossPercentage = (loss/(initial*quantity))*100;
     showOutput(`The loss is ${loss} and the percent is -${lossPercentage}%`);
     outputBox.style.color="red";
     }
     else if(initial<current){
         var profit = (current-initial)*quantity;
-        var profitPercentage = (profit/initial)*100;
+        var profitPercentage = (profit/(initial*quantity))*100;
     showOutput(`The profit is ${profit} and the percent is  ${profitPercentage}%`);
     outputBox.style.color="green";
     }
@@ -31,7 +31,16 @@ function clickHandler(){
     var ip=Number(initialPrice.value);
     var qty = Number(quantityOfStocks.value);
     var curr = Number(currentPrice.value);
-    calcProfitAndLoss(ip,qty,curr)
+
+    if(ip<=0 || qty <=0 || curr <=0){
+        outputBox.innerText="invalid input,enter values >0";
+        outputBox.style.color="gray";
+    }
+    else{
+
+
+
+    calcProfitAndLoss(ip,qty,curr)}
 }
 
 
